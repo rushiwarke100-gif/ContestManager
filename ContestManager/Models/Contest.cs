90e24b318a8547b6afcfde3774b2456efad30b3f
RUSHIKESH WARKE <RUSHIKESH WARKE@LAPTOP-5R8CBVGK>
2026-03-12 18:11:30 +0530

    Add project files.

---
+++ ContestManager/Models/Contest.cs
@@ -0,0 +1,12 @@
+﻿namespace ContestManager.Models
+{
+    public class Contest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AccessLevel { get; set; } // VIP, Normal
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ICollection<Question> Questions { get; set; }
+    }
+}
